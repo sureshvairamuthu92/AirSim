d8c2a9c64c5da0220ea92062923e3bf12b550fc7
Suresh Vairamuthu <suresh.murugesan@memryx.com>
2023-12-01 14:20:04 -0500

    modified setup and build script

--- build.sh
+++ build.sh
@@ -64,11 +64,11 @@ if [ "$(uname)" == "Darwin" ]; then
     export CXX="$(brew --prefix)/opt/llvm/bin/clang++"
 else
     if $gcc; then
-        export CC="gcc-8"
-        export CXX="g++-8"
+        export CC="gcc-11"
+        export CXX="g++-11"
     else
-        export CC="clang-8"
-        export CXX="clang++-8"
+        export CC="clang-11"
+        export CXX="clang++-11"
     fi
 fi
 
